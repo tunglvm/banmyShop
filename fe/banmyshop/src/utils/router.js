fcf1e57deec7eab00523b2d373417b7b2a46407a
tunglvm <dotunglam2k5@gmail.com>
2026-01-12 03:21:04 +0700

    debug router

--- fe/banmyshop/src/utils/router.js
+++ fe/banmyshop/src/utils/router.js
@@ -1,8 +1,8 @@
 //Sử dụng react router dom 
 //Giúp tạo website nhiều trang (multi-page logic) nhưng vẫn chạy như Single Page Application (SPA)
 
-export const ROUTER = {
+export const ROUTER = { //quản lý tất cả đường dẫn (route) ở 1 nơi duy nhất
     USER: {
-        HOME: "", //nested route
+        HOME: "", //index route (route mặc định)
     }
 }
