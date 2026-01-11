df2d85b29fcf32ada29c0e6390292f5efe22dbd8
tunglvm <dotunglam2k5@gmail.com>
2026-01-12 05:32:18 +0700

    header1

--- fe/banmyshop/src/pages/users/theme/header/index.js
+++ fe/banmyshop/src/pages/users/theme/header/index.js
@@ -1,10 +1,24 @@
 import {memo} from "react"; //PureComponent cho Function Component
 import "./style.scss";
+import { FaFacebookF } from "react-icons/fa6";
+import { GrInstagram } from "react-icons/gr";
+import { TbBrandShopee } from "react-icons/tb";
 
 const Header = () => {
     return (
         <div className = "header-top">
-            <div className="container" >Header</div>;
+            <div className="container">
+                <div className="row">
+                    <div className="col-6">Left</div>
+                    <div className="col-6 header-top-right">
+                        <ul>
+                            <li className="icon"> <FaFacebookF /> </li>
+                            <li className="icon"> <GrInstagram /> </li>
+                            <li className="icon"> <TbBrandShopee /> </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>;
         </div>
     );
         
