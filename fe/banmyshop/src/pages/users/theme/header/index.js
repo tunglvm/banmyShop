2681e2b2176d30ad735ba2022a1337cb2a2e1f11
tunglvm <dotunglam2k5@gmail.com>
2026-01-12 05:02:53 +0700

    test scss

--- fe/banmyshop/src/pages/users/theme/header/index.js
+++ fe/banmyshop/src/pages/users/theme/header/index.js
@@ -1,7 +1,13 @@
 import {memo} from "react"; //PureComponent cho Function Component
+import "./style.scss";
 
 const Header = () => {
-    return <h1>Header</h1>;
+    return (
+        <div className = "header-top">
+            <div className="container" >Header</div>;
+        </div>
+    );
+        
 };
 
 export default memo(Header); //tránh render lại khi props không thay đổi
